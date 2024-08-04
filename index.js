99c349927d2762d5bab9df3d5459e78f96d3758d
Adia Holt <adia.holt@gmail.com>
2024-08-04 10:46:45 -0500

    update files

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 console.log("hello world ");
+
+//Refrain
 console.log("Nah to the ah to the, no, no, no");
 console.log("My name is no, my sign is no, my number is no, uh");
 console.log("You need to let it go, uh");
 console.log("You need to let it go, uh");
 console.log("Need to let it go, uh");
+console.log("Nah to the ah to the, no, no, no");
+
+//Verse 1
+console.log("First you gonna say you ain't runnin' game");
+console.log("Thinking I'm believing every word");
+console.log("Call me beautiful, so original");
+console.log("Telling me I'm not like other girls");
+console.log("I was in my zone before you came along");
+console.log("Now I'm thinking maybe you should go");
+
+//Refrain 2
+console.log("All my ladies, listen up");
+console.log("If that boy ain't giving up");
+console.log("Lick your lips and swing your hips");
+console.log("Girl, all you gotta say is");
+
+//Refrain 1
+console.log("Nah to the ah to the, no, no, no");
+console.log("My name is no, my sign is no, my number is no, uh");
+console.log("You need to let it go, uh");
+console.log("You need to let it go, uh");
+console.log("Need to let it go, uh");
+console.log("Nah to the ah to the, no, no, no");
+
+
+//Verse 2
+console.log("Thank you in advance, I don't wanna dance (nope)");
+console.log("I don't need your hands all over me (no, no)");
+console.log("If I want a man, then I'ma get a man");
+console.log("But it's never my priority (hey)");
+console.log("I was in my zone, before you came along");
+console.log("Don't want you to take this personal");
