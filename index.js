9e2092ad74150dcaa3bb1c29e06e59f6ff126b75
Adia Holt <adia.holt@gmail.com>
2024-08-04 10:29:58 -0500

    song complete

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("hello world ");
+console.log("Nah to the ah to the, no, no, no");
+console.log("My name is no, my sign is no, my number is no, uh");
+console.log("You need to let it go, uh");
+console.log("You need to let it go, uh");
+console.log("Need to let it go, uh");
